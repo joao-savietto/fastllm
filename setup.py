8fe7a5f00834189f55b44f70ed40c6aa8a576cf6
João Savietto <joao.savietto@youtan.com.br>
2025-09-11 11:02:18 -0300

    chore: ver. 0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_requirements(file_path):
 
 setup(
     name="fastllm",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
